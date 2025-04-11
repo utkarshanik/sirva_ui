5e172677246d0e85a2d80d0c209d6a58c29f4758
utkarshanik <131257156+utkarshanik@users.noreply.github.com>
2025-04-11 18:08:59 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,GridModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sirva_ui';
+  public gridData: any[] = [
+    {
+      ProductID: 1,
+      ProductName: 'Chai',
+      Category: { CategoryName: 'Beverages' }
+    },
+    {
+      ProductID: 2,
+      ProductName: 'Chang',
+      Category: { CategoryName: 'Beverages' }
+    }
+  ];
 }
