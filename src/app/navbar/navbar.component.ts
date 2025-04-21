c06826b4ed75e4579337014bd4cc7d46b8aaa530
utkarshanik <131257156+utkarshanik@users.noreply.github.com>
2025-04-21 11:49:23 +0530

    Added top side panel

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { KENDO_CALENDAR, CalendarModule, KENDO_DATEINPUTS } from '@progress/kendo-angular-dateinputs';
 import { TabStripModule } from '@progress/kendo-angular-layout';
 import { HomeComponent } from "../home/home.component";
 import { DatamanageComponent } from '../datamanage/datamanage.component';
 import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
 import { ThemeService } from '../services/theme.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +16,7 @@ import { ThemeService } from '../services/theme.service';
     KENDO_DATEINPUTS, 
     HomeComponent,
     DatamanageComponent,
-    KENDO_BUTTONS
+    KENDO_BUTTONS,NgIf
   ],
   standalone: true,
   templateUrl: './navbar.component.html',
@@ -23,8 +24,9 @@ import { ThemeService } from '../services/theme.service';
 })
 export class NavbarComponent {
   constructor(private themeService: ThemeService) {}
-
+  isDarkMode: boolean = true;
   toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
     this.themeService.toggleDarkMode();
   }
 }
